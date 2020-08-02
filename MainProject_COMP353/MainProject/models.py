dab250f1c5bdbfcf654227b03b3027e71700e618
SeedrikMTL-dev <55291117+SeedrikMTL-dev@users.noreply.github.com>
2020-08-02 16:45:00 -0400

    Add files via upload
    Had to add flask_mail to Project Interpreter.
    Had to add 2 environment variables, values are in the Discord channel

--- MainProject_COMP353/MainProject/models.py
+++ MainProject_COMP353/MainProject/models.py
@@ -1,7 +1,8 @@
 # Code inspired from https://github.com/CoreyMSchafer/code_snippets/tree/master/Python/Flask_Blog/08-Posts/flaskblog
 
 from datetime import datetime
-from MainProject import db, login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from MainProject import db, login_manager, app
 from flask_login import UserMixin
 
 
@@ -16,6 +17,19 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
